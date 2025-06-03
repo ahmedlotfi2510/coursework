77d54b2ee99f9b63c341c3662e68288c3dcf180e
unknown <ahmedlotfi2501@gmail.com>
2025-06-03 18:33:12 -0400

    Day 6 Excercies -Ahmed

---
+++ week2/Excercises-Day6/Question6.R
@@ -0,0 +1,15 @@
+# Question 6.1.1
+1 - pnorm(650,560,57)
+
+# Question 6.1.2
+1 - pnorm(650,630,61)
+
+# Question 6.1.3
+tenth_percentile <- qnorm(0.1,560,57) #==> gives the 10%-percentile 
+ninty_percentile <- qnorm(0.9,560,57) #==> gives the 90%-percentile 
+# the final answer would be an interval of these two from 10 to 90 
+
+# Question 6.1.4
+tenth_percentile_new <- qnorm(0.1,630,61) #==> gives the 10%-percentile 
+ninty_percentile_new <- qnorm(0.9,630,61) #==> gives the 90%-percentile 
+# the final answer would be an interval of these two from 10 to 90 
