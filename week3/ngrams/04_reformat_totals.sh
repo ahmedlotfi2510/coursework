8076b0c6634a564abda6090a71605d8a29373516
unknown <ahmedlotfi2501@gmail.com>
2025-06-15 23:43:25 -0400

    ngrams project -Ahmed

--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -3,3 +3,8 @@
 # reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
+
+
+awk 'BEGIN { OFS = "\n" } { $1=$1; print }' googlebooks-eng-all-totalcounts-20120701.txt > total_counts.csv
+
+
